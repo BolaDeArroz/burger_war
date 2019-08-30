e8aa9dfa03e616f5d0e64dfd404da813f770b035
suzuki-shota001 <suzuki.shota001@jp.panasonic.com>
2019-08-30 10:42:12 +0900

    Update strategy

--- burger_war/scripts/attack_strategy.py
+++ burger_war/scripts/attack_strategy.py
@@ -18,15 +18,13 @@ class AttackStrategy():
         self.mov_sub = rospy.Subscriber("array", Float32MultiArray, self.move_callback)
 
     def init_vars(self, move, size, width, forced, t):
-        x, y, _ = calc_enemy_local(move, size, width)
-
         self.detection_data = True
         self.move_data = move
         self.size_data = size
         self.width_data = width
         self.forced = forced
         self.last_my_map = get_my_map(self.tf_listener, self.name)
-        self.last_enemy_local = (x, y)
+        self.last_enemy_local = calc_enemy_local(move, size, width)
         self.last_time = t
         self.detection_time = 0
 
@@ -42,9 +40,9 @@ class AttackStrategy():
             self.rate.sleep()
 
         if res == ATK_STRATEGY_ATTACK:
-            mx, my, ma = local_to_map(self.last_enemy_local, self.last_my_map)
+            enemy_map = local_to_map(self.last_enemy_local, self.last_my_map)
 
-            AttackBot().attack_war(mx, my, ma)
+            AttackBot().attack_war(enemy_map.p[0], enemy_map.p[1], enemy_map.a)
 
         return res
 
@@ -56,28 +54,30 @@ class AttackStrategy():
         if (dt < 0.001) or (self.last_my_map is None):
             return ATK_STRATEGY_CONTINUE
 
-        x, y, ds = calc_enemy_local(self.move_data, self.size_data, self.width_data)
+        enemy_local = calc_enemy_local(self.move_data, self.size_data, self.width_data)
 
-        vx, vy = calc_velocity((x, y), self.last_enemy_local, dt)
+        vx, vy = calc_velocity(enemy_local.carte, self.last_enemy_local.carte, dt)
 
-        self.last_enemy_local = (x, y)
+        self.last_enemy_local = enemy_local
         self.last_time = t
         self.detection_time += dt
+        print("[AttackStrategy]", self.last_my_map.p, self.last_enemy_local.polar)
 
         if (self.forced):
             print("[AttackStrategy] Attack (Forced)")
             return ATK_STRATEGY_ATTACK
 
-        elif (ds > 1.7):
-            print("[AttackStrategy] Retire (Distance)", ds)
+        elif (self.last_enemy_local.polar[0] > 2.0):
+            print("[AttackStrategy] Retire (Distance)", self.last_enemy_local.polar[0])
             return ATK_STRATEGY_RETIRE_DISTANCE
 
-        elif (not self.detection_data) or (vx * vx + vy * vy > 0.04):
+        #elif (not self.detection_data) or (vx * vx + vy * vy > 0.04):
+        elif (not self.detection_data):
             print("[AttackStrategy] Retire (Other)", self.detection_data, vx * vx + vy * vy)
             return ATK_STRATEGY_RETIRE_OTHER
 
-        elif (self.detection_time > 1.0) or (ds < 0.6):
-            print("[AttackStrategy] Attack", self.detection_time, ds)
+        elif (self.detection_time > 1.0) or (self.last_enemy_local.polar[0] < 0.6):
+            print("[AttackStrategy] Attack", self.detection_time, self.last_enemy_local.polar[0])
             return ATK_STRATEGY_ATTACK
 
         else:
@@ -91,13 +91,25 @@ class AttackStrategy():
         self.width_data = int(data.data[3])
 
 
+class LocalPoint():
+    def __init__(self, x, y, ds, th):
+        self.carte = x, y
+        self.polar = ds, th
+
+
+class MapInfo():
+    def __init__(self, x, y, a):
+        self.p = x, y
+        self.a = a
+
+
 def get_my_map(tf_listener, bot_name):
     try:
-        trans, quaternion = tf_listener.lookupTransform(bot_name + "/map", bot_name + "/base_link", rospy.Time())
+        ts, qt = tf_listener.lookupTransform(bot_name + "/map", bot_name + "/base_link", rospy.Time())
 
-        euler = tf.transformations.euler_from_quaternion(quaternion)
+        el = tf.transformations.euler_from_quaternion(qt)
 
-        return trans[0], trans[1], euler[2]
+        return MapInfo(ts[0], ts[1], el[2])
 
     except Exception as e:
         print("[AttackStrategy] Exception", e)
@@ -105,7 +117,9 @@ def get_my_map(tf_listener, bot_name):
 
 
 def calc_enemy_local(move, size, width):
-    area = float(size) / 2 * math.pi
+    r = float(size) / 2
+
+    area = r * r * math.pi
 
     ds = (-1.1492 * area + 1891.2) / 1000
     th = (math.pi / 2) - (math.pi / 6) * (float(move) / width * 2)
@@ -113,7 +127,7 @@ def calc_enemy_local(move, size, width):
     x = ds * math.cos(th)
     y = ds * math.sin(th)
 
-    return x, y, ds
+    return LocalPoint(x, y, ds, th)
 
 
 def calc_velocity(old, new, dt):
@@ -123,12 +137,13 @@ def calc_velocity(old, new, dt):
 
 
 def local_to_map(local, my_map):
-    ma = -my_map[2] + math.pi / 2
+    a = -my_map.a + math.pi / 2
 
-    mx = local[0] * math.cos(ma) + local[1] * math.sin(ma) + my_map[0]
-    my = local[1] * math.cos(ma) - local[0] * math.sin(ma) + my_map[1]
+    mx = local.carte[0] * math.cos(a) + local.carte[1] * math.sin(a) + my_map.p[0]
+    my = local.carte[1] * math.cos(a) - local.carte[0] * math.sin(a) + my_map.p[1]
+    ma = my_map.a - local.polar[1]
 
-    return mx, my, ma
+    return MapInfo(mx, my, ma)
 
 
 ATK_STRATEGY_CONTINUE = 0x00
