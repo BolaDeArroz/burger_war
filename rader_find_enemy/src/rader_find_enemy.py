5d798aaa5c716f0e32df810e751d542c95d00001
kim47-art <kiyama.yuta@jp.panasonic.com>
2020-02-11 21:59:00 +0900

    自己位置をパブリッシュするノードをpub_my_poseを追加

--- rader_find_enemy/src/rader_find_enemy.py
+++ rader_find_enemy/src/rader_find_enemy.py
@@ -8,6 +8,7 @@ from std_msgs.msg import Bool
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
 from visualization_msgs.msg import Marker
+from std_msgs.msg import Float32MultiArray
 import tf
 class rader_find_enemy:
     def __init__(self):
@@ -16,8 +17,8 @@ class rader_find_enemy:
         self.name = robot_name
 
         #s
-        self.scan_ranges=[]
-        self.scan_sub = rospy.Subscriber('/{}/scan'.format(self.name), LaserScan, self.scan_callback)
+        # self.scan_ranges=[]
+        # self.scan_sub = rospy.Subscriber('/{}/scan'.format(self.name), LaserScan, self.scan_callback)
 
         #self.global_occpacy_grid=[]
         #self.map_sub=rospy.Subscriber('/{}/move_base/global_costmap/costmap'.format(self.name), OccupancyGrid, self.global_costmap_callback)
@@ -32,18 +33,19 @@ class rader_find_enemy:
             print('[rader_find_enemy]', e)
         self.tf_listener.waitForTransform(self.name +"/map",self.name +"/base_link",rospy.Time(),rospy.Duration(4.0))
 
-        self.marker=Marker()
-        self.marker.header.frame_id="/"+self.name+"/map"
-        self.marker.ns = "basic_shapes"
-        self.marker.id = 0
-        self.marker.scale.x=self.marker.scale.y=self.marker.scale.z=0.1
-        self.marker.color.a=1.0        
-        self.marker.type=Marker.CUBE
-        self.marker.action = Marker.ADD
+        # self.marker=Marker()
+        # self.marker.header.frame_id="/"+self.name+"/map"
+        # self.marker.ns = "basic_shapes"
+        # self.marker.id = 0
+        # self.marker.scale.x=self.marker.scale.y=self.marker.scale.z=0.1
+        # self.marker.color.a=1.0        
+        # self.marker.type=Marker.CUBE
+        # self.marker.action = Marker.ADD
 
-        self.marker_pub = rospy.Publisher('enemy_marker',Marker,queue_size=1)
+        # self.marker_pub = rospy.Publisher('enemy_marker',Marker,queue_size=1)
 
-        self.enemy_is_back_pub=rospy.Publisher('is_enemy_back',Bool,queue_size=1)
+        #self.enemy_is_back_pub=rospy.Publisher('is_enemy_back',Flo,queue_size=1)
+        self.mypos_pub=rospy.Publisher('/mypos',Float32MultiArray,queue_size=1)
 
     def scan_callback(self, data):
         self.scan_ranges=data.ranges
@@ -56,27 +58,29 @@ class rader_find_enemy:
         while not rospy.is_shutdown():
             #map座標系の現在位置をtfから取得する
             my_position, my_orientation = self.tf_listener.lookupTransform(self.name +"/map", self.name +"/base_link",rospy.Time())
-            my_eular=tf.transformations.euler_from_quaternion(my_orientation)
-            total_chance=0.0
-            for rader_rad in range(120,240):
-                if self.scan_ranges[rader_rad] <1.0:
-                    rader_pos_x=my_position[0]+self.scan_ranges[rader_rad]*math.cos(my_eular[2]+math.pi*2*rader_rad/360)
-                    rader_pos_y=my_position[1]+self.scan_ranges[rader_rad]*math.sin(my_eular[2]+math.pi*2*rader_rad/360)
-                    self.marker.pose.position.x=rader_pos_x
-                    self.marker.pose.position.y=rader_pos_y
-                    self.marker.header.stamp = rospy.Time.now()
-                    self.marker.id = rader_rad
-                    one_chance,self.marker.color.b,self.marker.color.g,self.marker.color.r=self.calc_chance_of_enemy(rader_pos_x,rader_pos_y)
-                    total_chance+=one_chance
-                    self.marker.lifetime=rospy.Duration()
-                else:
-                    self.marker.color.b=self.marker.color.g=self.marker.color.r=0.0
-                self.marker_pub.publish(self.marker)       
-            print('[rader_find_enemy]total_chance=',total_chance)
-            if total_chance >10:
-                self.enemy_is_back_pub.publish(True)
-            else:
-                self.enemy_is_back_pub.publish(False)
+            pubmy_pos=Float32MultiArray(data=[my_position[0],my_position[1]])
+            self.mypos_pub.publish(pubmy_pos)
+            # my_eular=tf.transformations.euler_from_quaternion(my_orientation)
+            # total_chance=0.0
+            # for rader_rad in range(120,240):
+            #     if self.scan_ranges[rader_rad] <1.0:
+            #         rader_pos_x=my_position[0]+self.scan_ranges[rader_rad]*math.cos(my_eular[2]+math.pi*2*rader_rad/360)
+            #         rader_pos_y=my_position[1]+self.scan_ranges[rader_rad]*math.sin(my_eular[2]+math.pi*2*rader_rad/360)
+            #         self.marker.pose.position.x=rader_pos_x
+            #         self.marker.pose.position.y=rader_pos_y
+            #         self.marker.header.stamp = rospy.Time.now()
+            #         self.marker.id = rader_rad
+            #         one_chance,self.marker.color.b,self.marker.color.g,self.marker.color.r=self.calc_chance_of_enemy(rader_pos_x,rader_pos_y)
+            #         total_chance+=one_chance
+            #         self.marker.lifetime=rospy.Duration()
+            #     else:
+            #         self.marker.color.b=self.marker.color.g=self.marker.color.r=0.0
+            #     self.marker_pub.publish(self.marker)       
+            # print('[rader_find_enemy]total_chance=',total_chance)
+            # if total_chance >10:
+            #     self.enemy_is_back_pub.publish(True)
+            # else:
+            #     self.enemy_is_back_pub.publish(False)
             r.sleep()
 
     def calc_chance_of_enemy(self,x,y):
