deb5f1a376ae5310381ff238d177ef1dd8e36f46
BolaDeArroz <kawai.takayasu@jp.panasonic.com>
2020-03-01 18:47:55 +0900

    merge dev_takayasu

---
+++ burger_war/scripts/behavior_attack.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import actionlib
+import actionlib_msgs
+import math
+import rospy
+import smach
+import smach_ros
+import tf
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import Bool, Float32MultiArray, Int32MultiArray
+
+
+class behavior_attack(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['outcome'])
+
+        self.sm_sub = smach.StateMachine(outcomes=['outcome'])
+
+        with self.sm_sub:
+            func = CommonFunction()
+
+            smach.StateMachine.add('Selecting', Selecting(func), transitions={
+                'success': 'Moving',
+                'end': 'outcome'
+            })
+            smach.StateMachine.add('Moving', Moving(func), transitions={
+                'success': 'Reading',
+                'fail': 'Selecting',
+                'read': 'Selecting',
+                'end': 'outcome'
+            })
+            smach.StateMachine.add('Reading', Reading(func), transitions={
+                'success': 'Selecting',
+                'timeout': 'Selecting',
+                'end': 'outcome'
+            })
+
+        sis = smach_ros.IntrospectionServer(
+                'server_name', self.sm_sub, '/SM_ATTACK')
+
+        sis.start()
+
+    def execute(self, userdata):
+        self.sm_sub.execute()
+
+        return 'outcome'
+
+
+class CommonFunction:
+    def __init__(self):
+        self.is_stop_receive = False
+
+        self.score = []
+
+        self.enemy_pos_from_score = []
+
+        self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+
+        rospy.Subscriber(
+                'state_stop',
+                Bool,
+                self.stop_callback)
+        rospy.Subscriber(
+                'pub_score',
+                Int32MultiArray,
+                self.score_callback)
+        rospy.Subscriber(
+                'enemy_pos_from_score',
+                Float32MultiArray,
+                self.enemy_pos_from_score_callback)
+
+    def reset(self):
+        self.is_stop_receive = False
+
+    def is_data_exists(self):
+        # TODO: pub_scoreが実装されたら修正
+        # return (len(self.score) > 0 and
+        #         len(self.enemy_pos_from_score) > 0)
+        return (len(self.enemy_pos_from_score) > 0)
+
+    def stop_callback(self, data):
+        if data.data == True:
+            self.is_stop_receive = True
+
+    def score_callback(self, data):
+        self.score = data.data
+
+    def enemy_pos_from_score_callback(self, data):
+        self.enemy_pos_from_score = data.data
+
+    def check_stop(self):
+        result = self.is_stop_receive
+
+        self.is_stop_receive = False
+
+        return result
+
+    def check_score(self):
+        result = [i for i, e in enumerate(self.score) if e > 0]
+
+        return result
+
+    def check_enemy_pos_from_score(self):
+        return self.enemy_pos_from_score
+
+    def check_client(self):
+        return self.client.get_state()
+
+    def set_goal(self, x, y, yaw):
+        self.client.wait_for_server()
+
+        goal = MoveBaseGoal()
+
+
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = -y/1000.0 #NOTE:暫定対策
+        goal.target_pose.pose.position.y =  x/1000.0 #NOTE:暫定対策
+        q = tf.transformations.quaternion_from_euler(0, 0, yaw+math.pi/2)#NOTE:暫定対策
+
+        goal.target_pose.pose.orientation.x = q[0]
+        goal.target_pose.pose.orientation.y = q[1]
+        goal.target_pose.pose.orientation.z = q[2]
+        goal.target_pose.pose.orientation.w = q[3]
+
+        self.client.send_goal(goal)
+
+    def cancel_goal(self):
+        self.client.cancel_goal()
+
+
+class Selecting(smach.State):
+    def __init__(self, func):
+        smach.State.__init__(
+                self,
+                outcomes=['success', 'end'],
+                output_keys=['target'])
+
+        self.func = func
+
+    def execute(self, userdata):
+        r = rospy.Rate(RATE)
+
+        self.func.reset()
+
+        while not rospy.is_shutdown():
+            if self.func.check_stop():
+                return 'end'
+
+            if not self.func.is_data_exists():
+                continue
+
+            target = self.select()
+
+            if target is not None:
+                userdata.target = target
+
+                return 'success'
+
+            r.sleep()
+
+        return 'end'
+
+    def select(self):
+        # TODO: 判断材料に自己位置を導入
+        # TODO: できれば経路も考慮したい
+        # TODO: 履歴
+        costs = BASE_COSTS[:]
+
+        score = self.func.check_score()
+        enemy = self.func.check_enemy_pos_from_score()
+
+        for i in score:
+            costs[i] += K_MY_MARKER
+
+        for i, _ in enumerate(costs):
+            for j in NEAR_CELLS[i]:
+                costs[i] += enemy[j] * K_ENEMY_POS_FROM_SCORE
+
+        index = costs.index(min(costs))
+
+        return index if min(costs) < K_MY_MARKER else None
+
+
+class Moving(smach.State):
+    def __init__(self, func):
+        smach.State.__init__(
+                self,
+                outcomes=['success', 'fail', 'read', 'end'],
+                input_keys=['target'],
+                output_keys=['target'])
+
+        self.func = func
+
+    def execute(self, userdata):
+        r = rospy.Rate(RATE)
+
+        self.func.reset()
+        self.func.set_goal(*(POINTS[userdata.target]))
+
+        print(POINTS[userdata.target])#debug用TODO:あとで消す
+        while not rospy.is_shutdown():
+            if self.func.check_stop():
+                return 'end'
+
+            if not self.func.is_data_exists():
+                continue
+
+            result = self.check(userdata.target)
+
+            if result is not None:
+                self.func.cancel_goal()
+
+                return result
+
+            r.sleep()
+
+        return 'end'
+
+    def check(self, target):
+        score = self.func.check_score()
+
+        if target in score:
+            return 'read'
+
+        state = self.func.check_client()
+
+        if state == actionlib_msgs.msg.GoalStatus.PENDING:
+            return None
+        if state == actionlib_msgs.msg.GoalStatus.ACTIVE:
+            return None
+        if state == actionlib_msgs.msg.GoalStatus.RECALLED:
+            return None
+        if state == actionlib_msgs.msg.GoalStatus.REJECTED:
+            return 'fail'
+        if state == actionlib_msgs.msg.GoalStatus.PREEMPTED:
+            return 'fail'
+        if state == actionlib_msgs.msg.GoalStatus.ABORTED:
+            return 'fail'
+        if state == actionlib_msgs.msg.GoalStatus.SUCCEEDED:
+            return 'success'
+        if state == actionlib_msgs.msg.GoalStatus.LOST:
+            return 'fail'
+
+        return 'fail'
+
+
+class Reading(smach.State):
+    def __init__(self, func):
+        smach.State.__init__(
+                self,
+                outcomes=['success', 'timeout', 'end'],
+                input_keys=['target'])
+
+        self.func = func
+
+    def execute(self, userdata):
+        # TODO: 当たらない程度に移動
+        r = rospy.Rate(RATE)
+
+        self.func.reset()
+
+        start = rospy.Time.now()
+
+        while not rospy.is_shutdown():
+            if self.func.check_stop():
+                return 'end'
+
+            if not self.func.is_data_exists():
+                continue
+
+            if (rospy.Time.now() - start).to_sec() > TIMEOUT_READING:
+                return 'timeout'
+
+            result = self.check(userdata.target)
+
+            if result is not None:
+                return result
+
+            r.sleep()
+
+        return 'end'
+
+    def check(self, target):
+        score = self.func.check_score()
+
+        if target in score:
+            return 'success'
+
+        return None
+
+
+RATE = 10
+
+
+TIMEOUT_READING = 5
+
+
+K_MY_MARKER = 100
+
+
+K_ENEMY_POS_FROM_SCORE = 1
+
+
+POINTS = eval("""
+[
+        (-530,  760, -math.pi / 2),
+        ( 530,  760, -math.pi / 2),
+        (-530,  300,  math.pi / 2),
+        ( 530,  300,  math.pi / 2),
+        (   0,  300, -math.pi / 2),
+        (-300,    0, -math.pi),
+        ( 300,    0,  math.pi),
+        (   0, -300,  math.pi / 2),
+        (-530, -300, -math.pi / 2),
+        ( 530, -300, -math.pi / 2),
+        (-530, -760,  math.pi / 2),
+        ( 530, -760,  math.pi / 2)
+]
+""")
+
+
+BASE_COSTS = eval("""
+[
+        0.00, 0.00,
+        0.25, 0.25,
+        0.50, 0.50, 0.50, 0.50,
+        0.25, 0.25,
+        0.00, 0.00
+]
+""")
+
+
+NEAR_CELLS = eval("""
+[
+        [ 2,  3],
+        [ 4,  5],
+        [ 7,  8],
+        [ 9, 10],
+        [ 8,  9],
+        [ 8, 14],
+        [ 9, 15],
+        [14, 15],
+        [13, 14],
+        [15, 16],
+        [18, 19],
+        [20, 21]
+]
+""")
---
+++ burger_war/scripts/enemy_pos_from_lider.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import math
+import numpy as np
+
+import roslib
+import rospy
+from std_msgs.msg import Bool
+from nav_msgs.msg import OccupancyGrid
+from sensor_msgs.msg import LaserScan,PointCloud2,PointCloud
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
+from burger_war.msg import MyPose
+import tf
+from PIL import Image
+import os 
+import cv2
+from laser_geometry import LaserProjection
+from obstacle_detector.msg import Obstacles
+
+
+
+
+
+class enemy_pos_from_lider:
+    def __init__(self):
+        # bot name 
+        robot_name=''
+        self.name = robot_name
+
+        # /Obstaclesトピックサブスクライブ用
+        self.obstacles=Obstacles()
+        self.obstacles_sub = rospy.Subscriber('/{}/obstacles'.format(self.name), Obstacles, self.obstacle_callback)
+
+        # /敵位置トピックパブ用
+        self.pub_enemy_pos=rospy.Publisher('enemy_pos_from_lider',Point,queue_size=1)
+
+ 
+        self.marker=Marker()
+        self.marker.header.frame_id="/"+self.name+"/map"
+        self.marker.ns = "basic_shapes"
+        self.marker.id = 0
+        self.marker.scale.x=self.marker.scale.y=self.marker.scale.z=0.2
+        self.marker.color.a=1.0
+        self.marker.color.r=1.0
+        self.marker.type=Marker.CUBE
+        self.marker.action = Marker.ADD
+        self.enemy_marker_pub = rospy.Publisher('enemy_pos_from_lider_marker',Marker,queue_size=1)
+
+        #敵位置マップ用定数定義
+        self._MAP_OFFSET_X=-1.6770000
+        self._MAP_OFFSET_Y=-1.6970000
+
+
+    def obstacle_callback(self, data):
+        self.obstacles=data
+
+
+    def run(self):
+        r=rospy.Rate(5)
+        while not rospy.is_shutdown():
+            obstacles=self.obstacles
+            for obs in obstacles.circles:
+                enemy_pos=Point()
+                #横軸x,縦軸yの座標に戻す
+                enemy_pos.x=-obs.center.y
+                enemy_pos.y= obs.center.x
+                
+                #フィルタリング
+                #センターオブジェクトか
+                if(abs(enemy_pos.x) <=0.350 and abs(enemy_pos.y) <=0.350):
+                    continue
+                #コーナーオブジェクトか
+                if((abs(enemy_pos.y) >=0.420 and abs(enemy_pos.y) <=0.640) and (abs(enemy_pos.y) >=0.445 and abs(enemy_pos.y) <=0.615)):
+                    continue
+
+                self.marker.pose.position=obs.center
+                self.marker.header.stamp = rospy.Time.now()
+                self.marker.id = 1
+                self.marker.lifetime=rospy.Duration(0.5)
+                self.enemy_marker_pub.publish(self.marker)
+                self.pub_enemy_pos.publish(enemy_pos)
+            r.sleep()
+
+
+
+def main(args):
+    rospy.init_node('enemy_pos_from_lider', anonymous=True)
+    ra = enemy_pos_from_lider()
+    print('[enemy_pos_from_lider]initialized')
+    ra.run()
+
+if __name__=='__main__':
+    main(sys.argv)
