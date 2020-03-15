52f418fe8d49583700041012428335436c77f8eb
BolaDeArroz <kawai.takayasu@jp.panasonic.com>
2020-03-16 04:12:27 +0900

    merge dev_saedo

--- burger_war/scripts/enemy_pos_from_lider.py
+++ burger_war/scripts/enemy_pos_from_lider.py
@@ -36,19 +36,35 @@ class enemy_pos_from_lider:
         # /敵位置トピックパブ用
         self.pub_enemy_pos=rospy.Publisher('enemy_pos_from_lider',Point,queue_size=1)
 
- 
-        self.marker=Marker()
+        # /最終敵位置トピックパブ用
+        self.pub_last_enemy_pos=rospy.Publisher('enemy_pos_from_lider_last',Point,queue_size=1)
+        self.last_enemy_pos=Point(0,1.3,0)
 
+        # /敵位置マーカ
+        self.marker=Marker()
         self.marker.header.frame_id="map"
         self.marker.ns = "basic_shapes"
         self.marker.id = 0
-        self.marker.scale.x=self.marker.scale.y=self.marker.scale.z=0.25
+        self.marker.scale.x=self.marker.scale.y=self.marker.scale.z=0.20
         self.marker.color.a=1.0
         self.marker.color.r=1.0
         self.marker.type=Marker.CUBE
         self.marker.action = Marker.ADD
         self.enemy_marker_pub = rospy.Publisher('enemy_pos_from_lider_marker',Marker,queue_size=1)
 
+        # /最終敵位置マーカ
+        self.last_marker=Marker()
+        self.last_marker.header.frame_id="map"
+        self.last_marker.ns = "basic_shapes"
+        self.last_marker.id = 0
+        self.last_marker.scale.x=self.last_marker.scale.y=self.last_marker.scale.z=0.20
+        self.last_marker.color.a=1.0
+        self.last_marker.color.b=1.0
+        self.last_marker.type=Marker.CUBE
+        self.last_marker.action = Marker.ADD
+        self.last_marker.pose.position.x= 1.3
+        self.last_marker.pose.position.y= 0
+        self.enemy_last_marker_pub = rospy.Publisher('enemy_pos_from_lider_last_marker',Marker,queue_size=1)
 
     def obstacle_callback(self, data):
         self.obstacles=data
@@ -69,23 +85,44 @@ class enemy_pos_from_lider:
                 #敵とオブジェクトを見分けるマージン[m]。値が大きいほど、オブジェクトだと判定するエリアが大きくなる。
                 judge_enemy_mergin=0.0
                 #フィルタリング
+                #障害物の半径が10センチ以上か
+                if(obs.radius>=0.10):
+                    continue
                 #センターオブジェクトか
                 if(abs(enemy_pos.x) <=0.350+judge_enemy_mergin and abs(enemy_pos.y) <=0.350+judge_enemy_mergin):
+                #    print("is_center",enemy_pos)
                     continue
                 #コーナーオブジェクトか
-                if((abs(enemy_pos.y) >=0.420-judge_enemy_mergin and abs(enemy_pos.y) <=0.640+judge_enemy_mergin) and \
+                if((abs(enemy_pos.x) >=0.420-judge_enemy_mergin and abs(enemy_pos.x) <=0.640+judge_enemy_mergin) and \
                    (abs(enemy_pos.y) >=0.445-judge_enemy_mergin and abs(enemy_pos.y) <=0.615+judge_enemy_mergin)):
+                #    print("is_corner",enemy_pos)
                     continue
                 #壁か
                 if((abs(enemy_pos.y)+abs(enemy_pos.x)) >=1.650-judge_enemy_mergin):
+                #    print("is_wall",enemy_pos)
                     continue
 
+
+                self.pub_enemy_pos.publish(enemy_pos)
+                self.last_enemy_pos=enemy_pos                
+
+                #敵位置マーカー
                 self.marker.pose.position=obs.center
                 self.marker.header.stamp = rospy.Time.now()
                 self.marker.id = 1
+                self.marker.color.r=1.0
+                self.marker.color.b=0.0                
                 self.marker.lifetime=rospy.Duration(0.5)
                 self.enemy_marker_pub.publish(self.marker)
-                self.pub_enemy_pos.publish(enemy_pos)
+                self.last_marker=self.marker
+
+            self.pub_last_enemy_pos.publish(self.last_enemy_pos)
+
+            #最終敵位置マーカー
+            self.last_marker.id = 2
+            self.last_marker.color.r=0.0
+            self.last_marker.color.b=1.0
+            self.enemy_last_marker_pub.publish(self.last_marker)
             r.sleep()
 
 
